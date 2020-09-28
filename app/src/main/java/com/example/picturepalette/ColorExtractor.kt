8577b9d779f435f3bab18904e992c3891e748b68
axk4647 <arturokuang@gmail.com>
2020-09-28 14:59:01 -0400

    created color extractor class

---
+++ app/src/main/java/com/example/picturepalette/ColorExtractor.kt
@@ -0,0 +1,14 @@
+package com.example.picturepalette
+
+import android.graphics.Color
+
+class ColorExtractor constructor(
+    colors: List<Color>,
+    numColorsGenerated: Int,
+    maxIteration: Int
+) {
+
+    fun extract(): List<Color> {
+
+    }
+}
